5d1b83ace3980d2a518343480a9a2f52dc47842d
Daniel Alejandro Hernández Llerena <dhdezllerena94@gmail.com>
2023-04-24 18:01:20 +0100

    feat: RoomController

--- project/src/room/room.module.ts
+++ project/src/room/room.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RoomService } from './services/room.service';
+import { RoomController } from './controllers/room.controller';
 
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [RoomController],
   providers: [RoomService],
   exports: [RoomService],
 })
